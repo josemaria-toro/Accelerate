c6adc8aa029a41251a9ed54093aa915b8c68946a
José Mª Toro Bermejo <josemaria.toro@outlook.com>
2025-02-26 22:51:20 +0100

    Abstractions and memory cache library
    - Added base class for cache services.
    - Added support for cache services based on physical memory.

---
+++ cache/Memory/Cache/MemoryCacheServiceOptions.cs
@@ -0,0 +1,9 @@
+namespace Accelerate.Cache
+{
+    /// <summary>
+    /// Configuration options for memory cache service.
+    /// </summary>
+    public sealed class MemoryCacheServiceOptions : CacheServiceOptions
+    {
+    }
+}
---
+++ cache/UnitTests/readme.md
@@ -0,0 +1,9 @@
+# Accelerate cache libraries
+## UnitTests
+### v8.0.0
+
+- Added unit tests for cache services based on physical memory.
+
+```
+Zeta Technologies
+```
